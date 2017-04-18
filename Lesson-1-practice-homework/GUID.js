60b39531ff7988c4dcd582e125e85fd0a15db4eb
Vahan9797 <Vahan9797@gmail.com>
2017-04-18 17:04:18 +0300

    Changes since first upload: 1

--- Lesson-1-practice-homework/GUID.js
+++ Lesson-1-practice-homework/GUID.js
@@ -1,14 +1,23 @@
 function getGUID() {
+	function getCodeChunks(a) {
+		if (a === undefined || a < 1) {
+			a = 1;
+		}
+		return Math.random().toString(16).substr(2, 4*a);
+	}
 	var c = parseInt(document.getElementById("getAmount").value);
+	document.getElementById('textareaID').value = '';
 	var myGUID = '';
-	if (c < 1 || !c) {c = 1};
+	if (c < 1 || !c) {
+		c = 1
+	};
 	if (c >= 1) {
-		if (c > 100) {c = 100};
+		if (c > 100) {
+			c = 100
+		};
 	for (var i = 0; i < c; i++) {
-			for (var j = 0; j < 32; j++) {
-				if (j !=0 && j % 4 == 0) { myGUID += '-' };
-					myGUID += (Math.floor(Math.random()*15).toString(16));
-				}
+				myGUID += getCodeChunks(2) + '-' + getCodeChunks() + '-' + getCodeChunks() + 
+				'-' + getCodeChunks() + getCodeChunks(3);
 				document.getElementById('textareaID').value += myGUID + '\n';
 				myGUID = '';
 			}
