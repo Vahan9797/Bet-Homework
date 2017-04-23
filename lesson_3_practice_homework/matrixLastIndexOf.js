008b04d88ff4ab6189ca48e373889c0d046de17d
Vahan9797 <Vahan9797@gmail.com>
2017-04-23 11:38:54 +0400

    Changes made: 1

--- lesson_3_practice_homework/matrixLastIndexOf.js
+++ lesson_3_practice_homework/matrixLastIndexOf.js
@@ -1,32 +1,20 @@
 function matrixLastIndexOf(matrix, item) {
     var res;
-    matrix.reverse();
-    for (var i = 0; i < matrix.length; i++) {
+    for (var i = matrix.length - 1; i >= 0; i--) {
         if ((res = lastIndexOf(matrix[i], item)) !== -1) {
-            return [(matrix.length - i - 1), res];
+            return [i, res];
+        }
+    }
+};
+    
+    function lastIndexOf(arr, item) {
+        if (!arr.includes(item)) {
+            return -1;
+        } else {
+            for (var i = arr.length - 1; i >= 0; i--) {
+                if (arr[i] === item) {
+                    return i;
+                }
+            }
         }
     };
-
-    function lastIndexOf(arr, item) {
-        arr.reverse();
-        return arr.length - indexOf(arr, item) - 1;
-
-        function indexOf(arr, item) {
-            var  res, i = 0;
-            if (!arr.includes(item)) {
-                res = -1;
-            } else {
-                while (i < arr.length) {
-                    if (arr[i] === item) {
-                        res = i;
-                        break;
-                    } else {
-                        i++;
-                    };
-                }
-            };
-        return res;
-        };
-};
-
-}
