7bd798cbf9dc208fe4211004fc6bbef8bf00f07e
Vahan9797 <Vahan9797@gmail.com>
2017-04-23 11:45:35 +0400

    Changes made: 1

--- lesson_3_practice_homework/getRandomMatrix.js
+++ lesson_3_practice_homework/getRandomMatrix.js
@@ -1,14 +1,15 @@
 function getRandomMatrix(n, m) {
-    var matrix = [];
-    function getRandomInt(max) {
-        return parseInt(Math.random()*(++max));
-    };
+    var matrix = new Array(n);
     
     for (var i = 0; i < n; i++) {
-        matrix[i] = [];
+        matrix[i] = new Array(m);
         for (var j = 0; j < m; j++) {
             matrix[i][j] = getRandomInt(100);
         }
     };
     return matrix;
-}
+};
+
+    function getRandomInt(max) {
+        return Math.random()*(++max) | 0;
+    };
