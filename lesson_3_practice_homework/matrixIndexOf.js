12716c12680fb38e47618b15a5ddac1048704246
Vahan9797 <Vahan9797@gmail.com>
2017-04-23 11:13:35 +0400

    Changes made: 1

--- lesson_3_practice_homework/matrixIndexOf.js
+++ lesson_3_practice_homework/matrixIndexOf.js
@@ -3,24 +3,18 @@ function matrixIndexOf(matrix, item) {
     for (var i = 0; i < matrix.length; i++) {
         if ((res = indexOf(matrix[i], item)) !== -1) {
             return [i, res];
-            break;
         }
-    };
+    }
+};
 
-    function indexOf(arr, item) {
-        var  res, i = 0;
-        if (!arr.includes(item)) {
-            res = -1;
-        } else {
-            while (i < arr.length) {
-                if (arr[i] === item) {
-                    res = i;
-                    break;
-                } else {
-                    i++;
-                };
-            }
-        };
-        return res;
-    };
+function indexOf(arr, item) {
+    if (!arr.includes(item)) {
+        return -1;
+    } else {
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i] === item) {
+                return i;
+            } 
+        }
+    }
 };
