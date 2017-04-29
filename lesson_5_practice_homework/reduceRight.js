3af365c49ef84f9518a144ce67ebe056d971fbb5
Vahan Arakelyan <Vahan Arakelyan>
2017-04-29 16:06:25 +0400

    Changes made: 1

--- lesson_5_practice_homework/reduceRight.js
+++ lesson_5_practice_homework/reduceRight.js
@@ -2,7 +2,7 @@
     'use strict';
 
     Array.prototype.reduceRight = function(callback, initValue) {
-        var acc = (initValue) ? initValue : 0;
+        var acc = (initValue !== undefined) ? initValue : (this.some(function (a) { return typeof a === 'string' })) ? '' : 0;
         if(typeof callback === 'function') {
             for (var i in this) {
                 acc = callback(acc, this[this.length - i - 1], i, this);
