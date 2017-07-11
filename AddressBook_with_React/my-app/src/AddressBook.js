6ff25688b37dfe9b29bd5f9f9da0b3f3c6979bf6
Vahan9797 <Vahan9797@gmail.com>
2017-07-11 18:42:09 +0300

    Fixing Errors and changing concept.

--- AddressBook_with_React/my-app/src/AddressBook.js
+++ AddressBook_with_React/my-app/src/AddressBook.js
@@ -1,69 +1,47 @@
 import React, { Component } from 'react';
-import createGUID from './GUID';
+import AddressBookItem from './AddressBookItem';
+
 class AddressBook extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            onDelete: false,
             addressList: []
         };
-        this.totalContacts = this.state.addressList.length;
+    }
+
+    shouldComponentUpdate(nextProps) {
+        return nextProps.contacts && nextProps.contacts.length;
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.addContact(nextProps.contacts);
+    }
+
+    addContact(contactInfo) {
+        this.setState({
+            addressList: this.state.addressList.concat(new AddressBookItem(...contactInfo))
+        });
     }
 
     deleteContact(index) {
         this.state.addressList.splice(index, 1);
         this.setState({
-            onDelete: true,
             addressList: this.state.addressList
         });
     }
 
     render() {
-        let onDelete = this.state.onDelete;
-        this.state.onDelete = false;
         return (
             <div>
-            {!onDelete ? this.state.addressList = this.state.addressList.concat(
-                <p> {new AddressBookItem(...this.props.contacts).getInfo} 
-                <button onClick = {() => this.deleteContact(this.totalContacts)}>Delete</button></p>
-            ) : this.state.addressList}
+            {this.state.addressList.map((elem, index) => {
+                return ([
+                    <p>{elem.getInfo}</p>,
+                    <button onClick={() => this.deleteContact(index)}>Delete</button>
+                ])
+            })}
             </div>
         )
     }
-}
-
-class AddressBookItem {
-    constructor(lastName, firstName, address, email, postalCode, phoneNumber) {
-        this.id = createGUID();
-        this.lastName = lastName;
-        this.firstName = firstName;
-        this.address = address;
-        this.email = (AddressBookItem.emailValidation(email)) ? email : throwError`Email`;
-        this.postalCode = (AddressBookItem.postalCodeValidation(postalCode)) ? postalCode : throwError`Postal code`;
-        this.phoneNumber = (AddressBookItem.phoneNumberValidation(phoneNumber)) ? phoneNumber : throwError`Phone number`;
-    }
-
-    get getInfo() {
-        return `Info about person with full name: ${this.lastName} ${this.firstName}(` +
-        `Address: ${this.address} >>> ` + `Email: ${this.email} >>> ` +
-        `POST: ${this.postalCode} >>> ` + `Phone: ${this.phoneNumber})`;
-    }
-
-    static emailValidation(email) {
-        return /^\w+@[A-Z\a-z]+(\.[a-z]+){1,2}$/.test(email);
-    }
-
-    static postalCodeValidation(postalCode) {
-        return /^([0-9]{3,10})|([^\W_]]{6,8})$/.test(postalCode);
-    }
-
-    static phoneNumberValidation(phoneNumber) {
-        return /^\+[0-9]{3}(-*[0-9]{2}){4}$/.test(phoneNumber);
-    }
-}
-
-const throwError = (str = 'parameter') => {
-    throw new Error(`Your ${str} is not valid.`);
 }
 
 export default AddressBook;
