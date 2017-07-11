2ccae9878cd53b64480d6caaa4e361281e99e097
Vahan9797 <Vahan9797@gmail.com>
2017-07-11 18:42:59 +0300

    Fixing Errors and changing concept.

--- AddressBook_with_React/my-app/src/App.js
+++ AddressBook_with_React/my-app/src/App.js
@@ -4,37 +4,27 @@ import React, { Component } from 'react';
 import './App.css';
 import AddressBook from './AddressBook'
 
-
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      renderAddressBook: false,
       list: []
     };
   }
 
-  addContact() {
-    return this.setState({
-      renderAddressBook: true,
-      list: (['lastName', 'name', 'address', 'email', 'postNumber', 'phone'].map(elem => {
-        return document.getElementById(elem).value;
-      }))
-    })
-  }
-
   render() {
+    const renderList = [];
     return (
       <div className="App">
       <h1>Address Book</h1>
-      <label>Enter your name: <input type='text' id='name' /><br /><br /></label>
-      <label>Enter your last name: <input type='text' id='lastName' /><br /><br /></label>
-      <label>Enter your address: <input type='text' id='address'/><br /><br /></label>
-      <label>Enter your email: <input type='email' id='email' /><br /><br /></label>
-      <label>Enter your POST: <input type='text' id='postNumber' /><br /><br /></label>
-      <label>Enter your phone number: <input type='tel' id='phone' /><br /><br /></label>
-      <button onClick={() => this.addContact()}>Add</button>
-      {this.state.renderAddressBook ? <AddressBook contacts={this.state.list}></AddressBook> : null}
+      <label>Enter your name: <input type='text' ref={input => renderList.push(input)}/><br /><br /></label>
+      <label>Enter your last name: <input type='text' ref={input => renderList.push(input)}/><br /><br /></label>
+      <label>Enter your address: <input type='text' ref={input => renderList.push(input)}/><br /><br /></label>
+      <label>Enter your email: <input type='email' ref={input => renderList.push(input)}/><br /><br /></label>
+      <label>Enter your POST: <input type='text' ref={input => renderList.push(input)}/><br /><br /></label>
+      <label>Enter your phone number: <input type='tel' ref={input => renderList.push(input)}/><br /><br /></label>
+      <button onClick={() => this.setState({list: renderList.map(e => e.value)})}>Add</button>
+      <AddressBook contacts={this.state.list}></AddressBook>
       </div>
     )
   }
